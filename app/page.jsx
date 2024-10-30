7f5813df358480c0d8c72d842997b310516bd378
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-10-30 23:28:45 +0530

    small changes

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,12 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col mt-28">
       <h1 className="head_text text-center">
-        Discover & Share <br className="md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        Discover and Share <br className="md:hidden" />
+        <span className="orange_gradient text-center">AI-Driven Prompts</span>
       </h1>
       <p className="desc text-center">
-        PromptVault is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts.
+        PromptVault is an open-source platform for discovering, creating, and
+        sharing AI-driven prompts.
       </p>
       <Feed />
     </section>
